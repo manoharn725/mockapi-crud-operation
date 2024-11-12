2566618814a216881c41fa665f6e6cadd75e0173
manoharn725 <manoharnmanu725@gmail.com>
2024-11-12 13:07:10 +0530

    if you are in edit mode you can't go to create form

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useEditModeContext } from "../../context/EditMode/useEditModeContext";
 import "./index.scss";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const goToHome = () => {
-        navigate('/')
+  const navigate = useNavigate();
+  const { isEditMode, handleEditMode } = useEditModeContext();
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  const goToCreateForm = () => {
+    {
+      isEditMode ? "" : navigate("/create-form"), handleEditMode(false);
     }
+  };
   return (
     <div className="header__container">
-      <div onClick={goToHome} className="navbar__logo">Logo</div>
+      <div onClick={goToHome} className="navbar__logo">
+        Logo
+      </div>
       <div className="navbar__items">
         <Link to={"/"}>Students</Link>
-        <Link to={"/create-form"}>Form</Link>
+        <a to={"/create-form"} title={isEditMode? 'You are in editMode' : ''} onClick={goToCreateForm}>
+          Form
+        </a>
       </div>
     </div>
   );

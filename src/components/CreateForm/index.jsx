018e1629e851012183bf6c0ceedfb0e10550ddbd
manoharn725 <manoharnmanu725@gmail.com>
2025-03-26 15:43:42 +0530

    dynamic form

--- src/components/CreateForm/index.jsx
+++ src/components/CreateForm/index.jsx
@@ -9,10 +9,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEditModeContext } from "../../context/EditMode/useEditModeContext";
 
 const CreateForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { isEditMode, handleEditMode } = useEditModeContext();
 
   const { id } = useParams();
@@ -20,88 +23,90 @@ const CreateForm = () => {
   const { data: studentById } = useGetStudentByIdQuery(id, { skip: !id });
   const [updateStudent] = useUpdateStudentMutation();
 
+  const fieldType = [
+    {
+      label: "First Name",
+      type: "text",
+      name: "firstName",
+      placeholder: "First Name",
+      isRequired: true,
+    },
+    {
+      label: "Last Name",
+      type: "text",
+      name: "lastName",
+      placeholder: "Last Name",
+      isRequired: true,
+    },
+    {
+      label: "Email Address",
+      type: "email",
+      name: "email",
+      placeholder: "Email",
+      isRequired: true,
+    },
+    {
+      label: "Phone Number",
+      type: "number",
+      name: "phoneNumber",
+      placeholder: "Phone Number",
+      isRequired: true,
+    },
+  ];
+
   useEffect(() => {
     if (studentById) {
       handleEditMode(true);
-      setFirstName(studentById.firstName || "");
-      setLastName(studentById.lastName || "");
-      setEmail(studentById.email || "");
-      setPhoneNumber(studentById.phoneNumber || "");
+      setUser((prevUser) => ({ ...prevUser, ...studentById}));
     }
   }, [studentById, handleEditMode]);
 
   const navigate = useNavigate();
 
-  const handleFirstName = (e) => {
-    setFirstName(e.target.value);
-  };
-  const handleLastName = (event) => {
-    //just for reference
-    setLastName(event.target.value); //just for reference
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePhoneNumber = (e) => {
-    setPhoneNumber(Number(e.target.value)); //you can use + symbol instead of Number
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const studentData = { firstName, lastName, email, phoneNumber };
-    if (isEditMode) {
-      await updateStudent({ id, ...studentData });
-    } else {
-      await addStudent(studentData);
+    setIsSubmitting(true);
+
+    try {
+      if (isEditMode && id) {
+        await updateStudent({ id, ...user }).unwrap();
+      } else {
+        await addStudent(user).unwrap();
+      }
+      handleEditMode(false);
+      navigate("/");
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Something went wrong! Please try again.");
+    } finally {
+      setIsSubmitting(false);
     }
-    handleEditMode(false);
-    navigate("/");
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {fieldType.map(
+        ({ label, type, name, placeholder, isRequired }, index) => (
+          <div key={index} className="form__row">
+            <label htmlFor={name}>{label}</label>
+            <input
+              type={type}
+              name={name}
+              value={user[name] || ''}
+              onChange={handleChange}
+              placeholder={placeholder}
+              required={isRequired}
+            />
+          </div>
+        )
+      )}
       <div className="form__row">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={handleFirstName}
-          required
-        />
-      </div>
-      <div className="form__row">
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={handleLastName}
-          required
-        />
-      </div>
-      <div className="form__row">
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={handleEmail}
-          required
-        />
-      </div>
-      <div className="form__row">
-        <label htmlFor="phoneNumber">Phone Number</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          value={phoneNumber}
-          onChange={handlePhoneNumber}
-          required
-        />
-      </div>
-      <div className="form__row">
-        <button type="submit" value="Submit" onSubmit={handleSubmit}>
+        <button type="submit" value="Submit" disabled={isSubmitting}>
           {isEditMode ? "Update" : "Submit"}
         </button>
       </div>
